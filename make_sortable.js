ea45ff73f5f5193825a78d819d2e41ff1be0ffe9
Rodrigo Techera <rodrigo.webtools@gmail.com>
2018-08-10 13:23:41 -0300

    fixing bug with dragover class in container, simplifying the functionality

--- make_sortable.js
+++ make_sortable.js
@@ -1,79 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-	let draggingTarget = false;
-	
-	document.addEventListener('dragstart', function (e) {
-		if(e.target.draggable) {
-			e.dataTransfer.setData('text/html', null);
-
-			draggingTarget = e.target;
-			draggingTarget.previousDisplay = draggingTarget.style.display;
-		}
-	}, false);
-	
-	document.addEventListener('dragover', function (e) {
-		e.preventDefault(); //allowing drop event
-
-		if(draggingTarget) {
-			//dragHandler.style.top = (e.clientY||e.pageY) - Math.round(dragHandler.offsetHeight/2);
-			//dragHandler.style.left = (e.clientX||e.pageX) - Math.round(dragHandler.offsetWidth/2);
-
-			if(typeof e.target.sortable !== 'undefined') {
-				//dragging over sortable container
-				e.target.classList.add(e.target.sortable.overContainerClass);
-			} else if(typeof e.target.parentNode.sortable !== 'undefined' && e.target != draggingTarget) {
-				//dragging over sortable element
-				
-				const refPoint = elementPosition(e.target).top + Math.round(e.target.clientHeight/2);
-				if(e.clientY >= refPoint) {
-					//insert placeholder after
-					if(typeof e.target.nextSibling != draggingTarget) {
-						e.target.parentNode.insertBefore(draggingTarget, e.target.nextSibling);
-					}
-				} else {
-					//insert placeholder before
-					if(typeof e.target.previousSibling != draggingTarget) {
-						e.target.parentNode.insertBefore(draggingTarget, e.target);
-					}
-				}
-			}
-		}
-	}, false);
-	
-	document.addEventListener('dragleave', function (e) {
-		//console.log('dragleave', e);
-		//if(typeof e.target.sortable !== 'undefined') {
-		//	e.target.classList.remove(e.target.sortable.overContainerClass);
-		//}
-	}, false);
-
-	document.addEventListener('dragend', function (e) {
-		draggingTarget.parentNode.classList.remove(draggingTarget.parentNode.sortable.overContainerClass);
-
-		draggingTarget.style.display = draggingTarget.previousDisplay;
-		draggingTarget = false;
-		//tempElement = false;
-	}, false);
-
-	document.addEventListener('drop', function (e) {
-		e.preventDefault();
-
-		let sortableContainer = false;
-		if(typeof e.target.sortable !== 'undefined')
-			sortableContainer = e.target;
-		else if(typeof e.target.parentNode.sortable !== 'undefined')
-			sortableContainer = e.target.parentNode;
-	}, false);
-});
-
-function elementPosition(element) {
-	var rect = element.getBoundingClientRect(),
-	scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-	scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-	return {	top: rect.top + scrollTop,
-				left: rect.left + scrollLeft };
-}
-
+let draggingTarget = false;
 function makeSortable(options_param) {
 	const options = {
 		container : false,
@@ -87,9 +12,75 @@ function makeSortable(options_param) {
 			container.sortable = options;
 			container.classList.add(options.containerClass);
 			
+			container.addEventListener('dragstart', function (e) {
+				if(e.target.draggable) {
+					e.dataTransfer.setData('text/html', null);
+					e.dataTransfer.effectAllowed = 'move';
+
+					draggingTarget = e.target;
+					draggingTarget.previousDisplay = draggingTarget.style.display;
+				}
+			}, true);
+
+			container.addEventListener('dragend', function (e) {
+				if(this == e.target) {
+					this.classList.remove(this.sortable.overContainerClass);
+
+					draggingTarget.style.display = draggingTarget.previousDisplay;
+					draggingTarget = false;
+				}
+			}, false);
+
+			container.addEventListener('dragleave', function (e) {
+				if(this == e.target) {
+					this.classList.remove(this.sortable.overContainerClass);
+				}
+			}, false);
+
+			container.addEventListener('drop', function (e) {
+				//pass
+			}, false);
+
 			[].slice.call(container.children).forEach(element => {
 				element.draggable = true;
+				element.sortableContainer = container;
+
+				element.addEventListener('dragover', function (e) {
+					e.preventDefault(); //allowing drop event
+					
+					if(draggingTarget) {
+						//dragHandler.style.top = (e.clientY||e.pageY) - Math.round(dragHandler.offsetHeight/2);
+						//dragHandler.style.left = (e.clientX||e.pageX) - Math.round(dragHandler.offsetWidth/2);
+
+						this.parentNode.classList.add(this.parentNode.sortable.overContainerClass);
+						
+						if(this != draggingTarget) {
+							//dragging over sortable element
+							const refPoint = elementPosition(this).top + Math.round(this.clientHeight/2);
+							if(e.clientY >= refPoint) {
+								//insert placeholder after
+								if(this.nextSibling != draggingTarget) {
+									this.parentNode.insertBefore(draggingTarget, this.nextSibling);
+								}
+							} else {
+								//insert placeholder before
+								if(this.previousSibling != draggingTarget) {
+									this.parentNode.insertBefore(draggingTarget, this);
+								}
+							}
+						}
+					}
+				}, false);
 			});
 		});
 	}
+}
+
+function elementPosition(element) {
+	var rect = element.getBoundingClientRect(),
+	scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+	scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+	return {	top: rect.top + scrollTop,
+				left: rect.left + scrollLeft };
 }
